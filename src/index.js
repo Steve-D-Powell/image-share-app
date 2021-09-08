430a19bbaf29017b092d5017a7e83d131ae29130
stephen powell <stephen.powell@shopify.com>
2021-09-08 02:09:58 -0700

    added polaris dependancy

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@shopify/polaris/dist/styles.css';
+import {AppProvider} from '@shopify/polaris';
+import enTranslations from '@shopify/polaris/locales/en.json';
+
+function WrappedApp() {
+  return (
+    <AppProvider i18n={enTranslations}>
+      <App />
+    </AppProvider>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WrappedApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
